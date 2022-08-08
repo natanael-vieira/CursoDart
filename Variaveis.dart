eb798ab1387f140f7f74478ee905f9f48117d46b
Natanael Vieira <natanaelsales@live.com>
2022-08-08 19:08:18 -0300

    Create Variaveis.dart
    Exercício sobre variáveis Lógica com Dart.

---
+++ Variaveis.dart
@@ -0,0 +1,11 @@
+main() {
+  var hello = "Olá Mundo!";
+  hello = "Mudando o conteúdo da variável.";
+  hello =
+      "Quantas vezes eu quiser posso mudar, mas a última atribuição será a exibida.";
+  print(hello);
+
+  var numero = 2.22;
+  numero = 365;
+  print(numero);
+}
