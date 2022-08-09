fbcf8754e2a4dbecfbb32609f8a63263fdc41fc6
Natanael Vieira <natanaelsales@live.com>
2022-08-09 09:19:35 -0300

    Update CalculoImc.dart
    Utilizando funções com parâmetros

--- CalculoImc.dart
+++ CalculoImc.dart
@@ -1,7 +1,10 @@
 import 'dart:io';
 
 main() {
+  calculoDeImc();
+}
 
+calculoDeImc() {
   print("==========Digite seu peso==========");
   var textPeso = stdin.readLineSync();
   var peso = int.parse(textPeso!);
@@ -11,17 +14,21 @@ main() {
 
   var calcImc = peso / (altura * altura);
 
+  imprimirResultado(calcImc);
+}
+
+imprimirResultado(calcImc) {
   print("==================IMC==================");
 
   if (calcImc < 18.5) {
     print("Abaixo do Peso.");
-  } else if (calcImc > 18.5 && calcImc < 24.9){
+  } else if (calcImc > 18.5 && calcImc < 24.9) {
     print("Peso normal.");
-  } else if (calcImc > 25 && calcImc < 29.9){
+  } else if (calcImc > 25 && calcImc < 29.9) {
     print("Sobrepeso.");
-  } else if (calcImc > 30 && calcImc < 34.9){
+  } else if (calcImc > 30 && calcImc < 34.9) {
     print("Obesidade Grau 1.");
-  } else if (calcImc > 35 && calcImc < 39.9){
+  } else if (calcImc > 35 && calcImc < 39.9) {
     print("Obesidade Grau 2.");
   } else {
     print("Obesidade Grau 3.");
