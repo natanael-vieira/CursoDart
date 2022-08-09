2c16ee133c454215d3208f49da62a73bdeb9825f
Natanael Vieira <natanaelsales@live.com>
2022-08-09 06:36:17 -0300

    Update CalculoImc.dart
    Alterando parte do código de IMC

---
+++ CalculoImc.dart
@@ -0,0 +1,29 @@
+import 'dart:io';
+
+main() {
+
+  print("==========Digite seu peso==========");
+  var textPeso = stdin.readLineSync();
+  var peso = int.parse(textPeso!);
+  print("==========Digite sua altura==========");
+  var textAltura = stdin.readLineSync();
+  var altura = double.parse(textAltura!);
+
+  var calcImc = peso / (altura * altura);
+
+  print("==================IMC==================");
+
+  if (calcImc < 18.5) {
+    print("Abaixo do Peso.");
+  } else if (calcImc > 18.5 && calcImc < 24.9){
+    print("Peso normal.");
+  } else if (calcImc > 25 && calcImc < 29.9){
+    print("Sobrepeso.");
+  } else if (calcImc > 30 && calcImc < 34.9){
+    print("Obesidade Grau 1.");
+  } else if (calcImc > 35 && calcImc < 39.9){
+    print("Obesidade Grau 2.");
+  } else {
+    print("Obesidade Grau 3.");
+  }
+}
