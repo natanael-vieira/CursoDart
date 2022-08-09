0f1bf48000c81948b30b9ae45e730f141cf8857c
Natanael Vieira <natanaelsales@live.com>
2022-08-09 05:28:54 -0300

    Create Concatenacao.dart
    Exemplificando concatenação com Lógica em Dart.

---
+++ Concatenacao.dart
@@ -0,0 +1,13 @@
+main() {
+  var total = "Hello " + "World!";
+  print(total);
+
+  var text1 = "Abigail";
+  var text2 = " Vieira";
+
+  var textconc = text1 + text2;
+
+  print(textconc);
+
+  // Nos casos de concatenação de texto o único operador matemático utilizado é o de '+' porque ele não é apenas um operador matemático neste caso ele é o símbolo de concatenação também. 
+}
