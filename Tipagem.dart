2fc7cfeed27979c5d874ffaa766e4d08fc5f3931
Natanael Vieira <natanaelsales@live.com>
2022-08-09 17:42:21 -0300

    Create Tipagem.dart
    Somente uma explicação dos tipo de variáveis específicas para cada resultado ou entrada de informação específico, em Lógica com Dart.

---
+++ Tipagem.dart
@@ -0,0 +1,15 @@
+main() {
+  // As variáveis do tipo var são genéricas, elas assumem o que lhe foi atribuido independente do tipo de variável
+  var exemp1 = 1; // Aqui a variável foi atribuída como int
+  var exemp2 = 1.158; // Aqui a variável foi atribuída como double
+  var exemp3 = "Seu texto aqui."; // Aqui a variável foi atribuída como String
+  var isTrue = "exemp4"; // Aqui a variável assumiu um bool
+
+  // Porém podemos colocar o tipo que a variável vai assumir na atribuição da variável
+
+  int num1 = 1;
+  double num2 = 1.71;
+  String text1 = "Seu texto aqui!";
+  bool isTrue = true;
+  
+}
