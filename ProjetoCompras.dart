4b58b5b05717badbaf72d288463177f101e1a1df
Natanael Vieira <natanaelsales@live.com>
2022-08-10 18:48:40 -0300

    Update ProjetoCompras.dart
    Alteração na forma como será impresso a informação adicionada em lista.

--- ProjetoCompras.dart
+++ ProjetoCompras.dart
@@ -10,8 +10,12 @@ main() {
       print("===== Lista Pronta =====");
       condiction = false;
     } else if (text == "imprimir") {
-      print(products);
-      print("\n");
+      for (var i = 1; i < products.length; i++) {
+        /*print(products);
+        print("\n");*/
+        // uma maneira diferente de imprimir é colocando um for e usando as condições do for para mostrar os itens em lista crescente;
+        print("Item $i - ${products[i]}");
+      }
     } else {
       products.add(text!);
       print("\x1B[2J\x1B[0;0H"); //Comando para fazer a limpeza da tela;
