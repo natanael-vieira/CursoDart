b07c18baa805c51ea26300925ddb53445b1806e8
Natanael Vieira <natanaelsales@live.com>
2022-08-11 06:00:28 -0300

    Update Maps.dart
    Tipando o map, incluindo ali o tipo da key e dos dados como String e dynamic, respectivamente.

--- Maps.dart
+++ Maps.dart
@@ -1,12 +1,12 @@
 main() {
-  var maps = {
+  Map<String, dynamic> maps = { // Tipando o map, dentro do '<>' (generic), colocamos o tipo da key e separado por vírgula o tipo do dado que neste caso por ser de diferentes tipos é colocado o dynamic por ser dinâmico os dados.
     "nome": "Natanael Vieira",
     "idade": 34,
     "cidade": "Palhoça",
     "estado": "Santa Catarina",
   };
 
-  print(maps["idade"]);
+  print(maps["estado"]);
 }
 
 //Uma forma mais simples de organizar dados vindo da internet é criando maps, para que ele organize os dados e você consiga fazer um print, por exemplo, de um dado específico, mas que você não conhece o índice dele como é feito dentro de uma List, então só declarar no print a 'key' que você quer imprimir e ela será exibida. 
