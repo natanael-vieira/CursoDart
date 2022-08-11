ce6e4f92f38251ef65dd4ea10d5f4095cdc11d0c
Natanael Vieira <natanaelsales@live.com>
2022-08-11 05:11:58 -0300

    Update ProjetoCompras.dart
    Incluindo no código a possibilidade da remoção de qualquer item da lista.

--- ProjetoCompras.dart
+++ ProjetoCompras.dart
@@ -16,6 +16,15 @@ main() {
         // uma maneira diferente de imprimir é colocando um for e usando as condições do for para mostrar os itens em lista crescente;
         print("Item $i - ${products[i]}");
       }
+    } else if (text == "remover") {
+      print("===== Qual Item Deseja Remover? =====");
+      for (var i = 1; i < products.length; i++) {
+        print("Item $i - ${products[i]}");
+      }
+      var input = stdin.readLineSync();
+      var item = int.parse(input!);
+      products.removeAt(item);
+      print("===== Item Removido =====");
     } else {
       products.add(text!);
       print("\x1B[2J\x1B[0;0H"); //Comando para fazer a limpeza da tela;
