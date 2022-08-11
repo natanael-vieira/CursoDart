251b9e43d79ed3315953350afe22daf1f0d9e5ac
Natanael Vieira <natanaelsales@live.com>
2022-08-11 05:50:33 -0300

    Create Maps.dart
    Criando e utilizando maps em Lógica com Dart.

---
+++ Maps.dart
@@ -0,0 +1,12 @@
+main() {
+  var maps = {
+    "nome": "Natanael Vieira",
+    "idade": 34,
+    "cidade": "Palhoça",
+    "estado": "Santa Catarina",
+  };
+
+  print(maps["idade"]);
+}
+
+//Uma forma mais simples de organizar dados vindo da internet é criando maps, para que ele organize os dados e você consiga fazer um print, por exemplo, de um dado específico, mas que você não conhece o índice dele como é feito dentro de uma List, então só declarar no print a 'key' que você quer imprimir e ela será exibida. 
